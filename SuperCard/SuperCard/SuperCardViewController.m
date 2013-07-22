9105b9bcdd62ef886f353338061a10525a9f90e6
Alden Quimby <aldenquimby@gmail.com>
2013-07-22 12:44:45 -0700

    through lecture 6

--- SuperCard/SuperCard/SuperCardViewController.m
+++ SuperCard/SuperCard/SuperCardViewController.m
@@ -8,13 +8,58 @@
 
 #import "SuperCardViewController.h"
 #import "PlayingCardView.h"
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
 
 @interface SuperCardViewController ()
 
 @property (weak, nonatomic) IBOutlet PlayingCardView *playingCardView;
+@property (strong, nonatomic) Deck *deck;
 
 @end
 
 @implementation SuperCardViewController
+
+- (Deck *)deck
+{
+    if (!_deck) _deck = [[PlayingCardDeck alloc] init];
+    return _deck;
+}
+
+- (void)setPlayingCardView:(PlayingCardView *)playingCardView
+{
+    _playingCardView = playingCardView;
+    [self drawRandomPlayingCard];
+    [playingCardView addGestureRecognizer:[[UIPinchGestureRecognizer alloc] initWithTarget:playingCardView action:@selector(pinch:)]];
+}
+
+- (void)drawRandomPlayingCard
+{
+    Card *card = [self.deck drawRandomCard];
+    
+    if (![card isKindOfClass:[PlayingCard class]])
+    {
+        return;
+    }
+    
+    PlayingCard *playingCard = (PlayingCard *)card;
+    self.playingCardView.rank = playingCard.rank;
+    self.playingCardView.suit = playingCard.suit;
+}
+
+- (IBAction)swipe:(id)sender
+{
+    [UIView transitionWithView:self.playingCardView
+                      duration:0.5
+                       options:UIViewAnimationOptionTransitionFlipFromLeft
+                    animations:^{
+                        if (!self.playingCardView.faceUp)
+                        {
+                            [self drawRandomPlayingCard];
+                        }
+                        self.playingCardView.faceUp = !self.playingCardView.faceUp;
+                    }
+                    completion:NULL];
+}
 
 @end
